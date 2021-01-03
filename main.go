1af2c7a6f251da71ad907709ee103be84b3083b5
Andrzej Białecki <kaszanas@interia.pl>
2021-01-03 04:15:23 +0100

    Added batch processing of input directory
    Also added check for extension name of the files that are going to be processed.

--- main.go
+++ main.go
@@ -15,117 +15,121 @@ import (
 
 func main() {
 
-	// Getting list of files from input directory:
-	listOfInputFiles := listFiles("./DEMOS/Input")
+	// Settings:
+	inputDirectory := "./DEMOS/Input"
+	outputDirectory := "./DEMOS/Output"
 
-	for _, file := range listOfInputFiles {
-		fmt.Println(file.Name())
-		absoluteReplayFilepath, err := filepath.Abs(file.Name())
+	// Getting absolute path to input directory:
+	absolutePathInputDirectory, _ := filepath.Abs(inputDirectory)
+	absolutePathOutputDirectory, _ := filepath.Abs(outputDirectory)
+	// Getting list of absolute paths for files from input directory:
+	listOfInputFiles := listReplayFiles(absolutePathInputDirectory)
+
+	for _, replayFile := range listOfInputFiles {
+
+		// fmt.Println(absoluteReplayFilepath)
+		// replayFilepath := "./DEMOS/Input/11506446_1566325366_8429955.SC2Replay"
+
+		fmt.Println(replayFile)
+
+		replayData, err := rep.NewFromFile(replayFile)
 		if err != nil {
-			fmt.Printf("Failed to provide absolute_path: %v\n", err)
-		} else {
-			// fmt.Println(absoluteReplayFilepath)
-			// replayFilepath := "./DEMOS/Input/11506446_1566325366_8429955.SC2Replay"
-
-			replayFile, err := rep.NewFromFile(absoluteReplayFilepath)
-			if err != nil {
-				fmt.Printf("Failed to open file: %v\n", err)
-				return
-			}
-			defer replayFile.Close()
-
-			header := replayFile.Header.String()
-			details := replayFile.Details.String()
-			initData := replayFile.InitData.String()
-			attrEvts := replayFile.AttrEvts.String()
-			metadata := replayFile.Metadata.String()
-
-			PIDPlayerDescMap := replayFile.TrackerEvts.PIDPlayerDescMap
-			ToonPlayerDescMap := replayFile.TrackerEvts.ToonPlayerDescMap
-
-			// Creating lists of strings for later use in generating JSON out of the replay data:
-			var gameEventStrings []string
-			for _, gameEvent := range replayFile.GameEvts {
-				gameEventStrings = append(gameEventStrings, gameEvent.String())
-			}
-
-			var messageEventStrings []string
-			for _, messageEvent := range replayFile.MessageEvts {
-				messageEventStrings = append(messageEventStrings, messageEvent.String())
-			}
-
-			var trackerEventStrings []string
-			for _, trackerEvent := range replayFile.TrackerEvts.Evts {
-				trackerEventStrings = append(trackerEventStrings, trackerEvent.String())
-			}
-
-			// These structures are handled differently as it is a Map without .String() method:
-			var PIDPlayerDescMapStrings []string
-			for PIDPlayerDescKey, PIDPlayerDescValue := range PIDPlayerDescMap {
-
-				// Converting ID to string:
-				playerNumber := strconv.FormatInt(PIDPlayerDescKey, 10)
-
-				// Converting struct to JSON:
-				playerDescInformation, err := json.Marshal(PIDPlayerDescValue)
-
-				if err != nil {
-					panic(err)
-				}
-
-				// Putting everything together:
-				PIDPlayerDescMapStrings = append(PIDPlayerDescMapStrings, "\""+playerNumber+"\": "+string(playerDescInformation))
-			}
-
-			var ToonPlayerDescMapStrings []string
-			for ToonPlayerDescKey, ToonPlayerDescValue := range ToonPlayerDescMap {
-
-				// Converting ID to string:
-				playerToon := ToonPlayerDescKey
-
-				// Converting struct to JSON:
-				playerDescInformation, err := json.Marshal(ToonPlayerDescValue)
-
-				if err != nil {
-					panic(err)
-				}
-
-				// Putting everything together:
-				ToonPlayerDescMapStrings = append(ToonPlayerDescMapStrings, "\""+playerToon+"\": "+string(playerDescInformation))
-			}
-
-			// Booleans saying if processing had any errors
-			gameEvtsErr := strconv.FormatBool(replayFile.GameEvtsErr)
-			messageEvtsErr := strconv.FormatBool(replayFile.MessageEvtsErr)
-			trackerEvtsErr := strconv.FormatBool(replayFile.TrackerEvtsErr)
-
-			// Crezting JSON structure by hand:
-			var strBuilder strings.Builder
-			fmt.Fprintf(&strBuilder, "{\n")
-			fmt.Fprintf(&strBuilder, "  \"header\": %s,\n", header)
-			fmt.Fprintf(&strBuilder, "  \"initData\": %s,\n", initData)
-			fmt.Fprintf(&strBuilder, "  \"details\": %s,\n", details)
-			fmt.Fprintf(&strBuilder, "  \"attrEvts\": %s,\n", attrEvts)
-			fmt.Fprintf(&strBuilder, "  \"metadata\": %s,\n", metadata)
-			fmt.Fprintf(&strBuilder, "  \"gameEvtsErr\": %s,\n", gameEvtsErr)
-			fmt.Fprintf(&strBuilder, "  \"messageEvtsErr\": %s,\n", messageEvtsErr)
-			fmt.Fprintf(&strBuilder, "  \"trackerEvtsErr\": %s,\n", trackerEvtsErr)
-			fmt.Fprintf(&strBuilder, "  \"messageEventsStrings\": [%s],\n", strings.Join(messageEventStrings, ",\n"))
-			fmt.Fprintf(&strBuilder, "  \"gameEventStrings\": [%s],\n", strings.Join(gameEventStrings, ",\n"))
-			fmt.Fprintf(&strBuilder, "  \"trackerEventStrings\": [%s],\n", strings.Join(trackerEventStrings, ",\n"))
-			fmt.Fprintf(&strBuilder, "  \"PIDPlayerDescMap\": {%s},\n", strings.Join(PIDPlayerDescMapStrings, ",\n"))
-			fmt.Fprintf(&strBuilder, "  \"ToonPlayerDescMap\": {%s},\n", strings.Join(ToonPlayerDescMapStrings, ",\n"))
-			fmt.Fprintf(&strBuilder, "  \"gameEvtsErr\": %s", gameEvtsErr+",\n")
-			fmt.Fprintf(&strBuilder, "  \"messageEvtsErr\": %s", messageEvtsErr+",\n")
-			fmt.Fprintf(&strBuilder, "  \"trackerEvtsErr\": %s", trackerEvtsErr+"\n")
-			fmt.Fprintf(&strBuilder, "  ")
-			fmt.Fprintf(&strBuilder, "}")
-
-			// Writing JSON file:
-			_ = ioutil.WriteFile("./DEMOS/Output/11506446_1566325366_8429955.json", []byte(strBuilder.String()), 0644)
-
-			// TODO: every event
-
+			fmt.Printf("Failed to open file: %v\n", err)
+			return
 		}
+		defer replayData.Close()
+
+		header := replayData.Header.String()
+		details := replayData.Details.String()
+		initData := replayData.InitData.String()
+		attrEvts := replayData.AttrEvts.String()
+		metadata := replayData.Metadata.String()
+
+		PIDPlayerDescMap := replayData.TrackerEvts.PIDPlayerDescMap
+		ToonPlayerDescMap := replayData.TrackerEvts.ToonPlayerDescMap
+
+		// Creating lists of strings for later use in generating JSON out of the replay data:
+		var gameEventStrings []string
+		for _, gameEvent := range replayData.GameEvts {
+			gameEventStrings = append(gameEventStrings, gameEvent.String())
+		}
+
+		var messageEventStrings []string
+		for _, messageEvent := range replayData.MessageEvts {
+			messageEventStrings = append(messageEventStrings, messageEvent.String())
+		}
+
+		var trackerEventStrings []string
+		for _, trackerEvent := range replayData.TrackerEvts.Evts {
+			trackerEventStrings = append(trackerEventStrings, trackerEvent.String())
+		}
+
+		// These structures are handled differently as it is a Map without .String() method:
+		var PIDPlayerDescMapStrings []string
+		for PIDPlayerDescKey, PIDPlayerDescValue := range PIDPlayerDescMap {
+
+			// Converting ID to string:
+			playerNumber := strconv.FormatInt(PIDPlayerDescKey, 10)
+
+			// Converting struct to JSON:
+			playerDescInformation, err := json.Marshal(PIDPlayerDescValue)
+
+			if err != nil {
+				panic(err)
+			}
+
+			// Putting everything together:
+			PIDPlayerDescMapStrings = append(PIDPlayerDescMapStrings, "\""+playerNumber+"\": "+string(playerDescInformation))
+		}
+
+		var ToonPlayerDescMapStrings []string
+		for ToonPlayerDescKey, ToonPlayerDescValue := range ToonPlayerDescMap {
+
+			// Converting ID to string:
+			playerToon := ToonPlayerDescKey
+
+			// Converting struct to JSON:
+			playerDescInformation, err := json.Marshal(ToonPlayerDescValue)
+
+			if err != nil {
+				panic(err)
+			}
+
+			// Putting everything together:
+			ToonPlayerDescMapStrings = append(ToonPlayerDescMapStrings, "\""+playerToon+"\": "+string(playerDescInformation))
+		}
+
+		// Booleans saying if processing had any errors
+		gameEvtsErr := strconv.FormatBool(replayData.GameEvtsErr)
+		messageEvtsErr := strconv.FormatBool(replayData.MessageEvtsErr)
+		trackerEvtsErr := strconv.FormatBool(replayData.TrackerEvtsErr)
+
+		// Crezting JSON structure by hand:
+		var strBuilder strings.Builder
+		fmt.Fprintf(&strBuilder, "{\n")
+		fmt.Fprintf(&strBuilder, "  \"header\": %s,\n", header)
+		fmt.Fprintf(&strBuilder, "  \"initData\": %s,\n", initData)
+		fmt.Fprintf(&strBuilder, "  \"details\": %s,\n", details)
+		fmt.Fprintf(&strBuilder, "  \"attrEvts\": %s,\n", attrEvts)
+		fmt.Fprintf(&strBuilder, "  \"metadata\": %s,\n", metadata)
+		fmt.Fprintf(&strBuilder, "  \"gameEvtsErr\": %s,\n", gameEvtsErr)
+		fmt.Fprintf(&strBuilder, "  \"messageEvtsErr\": %s,\n", messageEvtsErr)
+		fmt.Fprintf(&strBuilder, "  \"trackerEvtsErr\": %s,\n", trackerEvtsErr)
+		fmt.Fprintf(&strBuilder, "  \"messageEventsStrings\": [%s],\n", strings.Join(messageEventStrings, ",\n"))
+		fmt.Fprintf(&strBuilder, "  \"gameEventStrings\": [%s],\n", strings.Join(gameEventStrings, ",\n"))
+		fmt.Fprintf(&strBuilder, "  \"trackerEventStrings\": [%s],\n", strings.Join(trackerEventStrings, ",\n"))
+		fmt.Fprintf(&strBuilder, "  \"PIDPlayerDescMap\": {%s},\n", strings.Join(PIDPlayerDescMapStrings, ",\n"))
+		fmt.Fprintf(&strBuilder, "  \"ToonPlayerDescMap\": {%s},\n", strings.Join(ToonPlayerDescMapStrings, ",\n"))
+		fmt.Fprintf(&strBuilder, "  \"gameEvtsErr\": %s", gameEvtsErr+",\n")
+		fmt.Fprintf(&strBuilder, "  \"messageEvtsErr\": %s", messageEvtsErr+",\n")
+		fmt.Fprintf(&strBuilder, "  \"trackerEvtsErr\": %s", trackerEvtsErr+"\n")
+		fmt.Fprintf(&strBuilder, "  ")
+		fmt.Fprintf(&strBuilder, "}")
+
+		_, replayFilename := filepath.Split(replayFile)
+		finalFilename := strings.TrimSuffix(replayFilename, filepath.Ext(replayFilename)) + ".json"
+		// Writing JSON file:
+		_ = ioutil.WriteFile(filepath.Join(absolutePathOutputDirectory, finalFilename), []byte(strBuilder.String()), 0644)
+
 	}
 }
--- path_utils.go
+++ path_utils.go
@@ -3,16 +3,27 @@ package main
 import (
 	"io/ioutil"
 	"log"
-	"os"
+	"path/filepath"
 )
 
-func listFiles(path string) []os.FileInfo {
+func listReplayFiles(inputPath string) []string {
 
-	files, err := ioutil.ReadDir(path)
+	files, err := ioutil.ReadDir(inputPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return files
+	var listOfReplayFiles []string
+	for _, file := range files {
+		filename := file.Name()
+		fileExtension := filepath.Ext(filename)
+		if fileExtension != ".SC2Replay" {
+		} else {
+			absoluteReplayPath := filepath.Join(inputPath, filename)
+			listOfReplayFiles = append(listOfReplayFiles, absoluteReplayPath)
+		}
+	}
+
+	return listOfReplayFiles
 
 }
