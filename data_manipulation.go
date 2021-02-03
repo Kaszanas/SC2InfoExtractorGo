225ba8be983544bbf09c3e9f3af358226e430e8c
Andrzej Białecki <kaszanas@interia.pl>
2021-02-03 16:52:01 +0100

    Defined TODOs

---
+++ data_manipulation.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/icza/s2prot/rep"
+
+func deleteUnusedObjects(replayData *rep.Rep) *rep.Rep {
+
+	// TODO: Clear the objects that will not be used in the final JSON
+
+	return replayData
+}
+
+func anonymizeReplayData(replayData *rep.Rep) *rep.Rep {
+
+	// TODO: Anonymize the information about players and chat events
+
+	return replayData
+}
