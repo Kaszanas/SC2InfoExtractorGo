c552e83e51c3502e549b2e0430bc6c7c896e5ed6
Andrzej Białecki <kaszanas@interia.pl>
2021-02-06 04:52:36 +0100

    Further cleaning type drafts, found string builder bug

--- custom_types/cleaned_replay.go
+++ custom_types/cleaned_replay.go
@@ -1,11 +1,21 @@
 package main
 
+import (
+	"github.com/icza/s2prot"
+	"github.com/icza/s2prot/rep"
+)
+
 type CleanedReplay struct {
-	header         CleanedHeader
-	initData       CleanedInitData
-	details        CleanedDetails
-	metadata       CleanedMetadata
-	gameEvtsErr    bool
-	messageEvtsErr bool
-	trackerEvtsErr bool
+	header            CleanedHeader
+	initData          CleanedInitData
+	details           CleanedDetails
+	metadata          CleanedMetadata
+	messageEvents     []s2prot.Event
+	gameEvents        []s2prot.Event
+	trackerEvents     []s2prot.Event
+	PIDPlayerDescMap  map[string]*rep.PlayerDesc
+	toonPlayerDescMap map[string]*rep.PlayerDesc
+	gameEvtsErr       bool
+	messageEvtsErr    bool
+	trackerEvtsErr    bool
 }
