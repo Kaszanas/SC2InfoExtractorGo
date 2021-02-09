8bcb6b0328fa004ff40cde659d34605af4f42d45
Andrzej Białecki <kaszanas@interia.pl>
2021-02-09 05:00:05 +0100

    Changed project structure

---
+++ dataproc/anonymize.go
@@ -0,0 +1,16 @@
+package dataproc
+
+import (
+	data "github.com/Kaszanas/GoSC2Science/datastruct"
+)
+
+// TODO: Introduce logging.
+
+// Anonymizes the replay and returns an error boolean
+func anonymizeReplayData(replayData *data.CleanedReplay) bool {
+
+	// TODO: Anonymize the information about players.
+	// This needs to be done by calling some external file and / or memory which will be holding persistent information about all of the players.
+
+	return true
+}
--- dataproc/cleaning.go
+++ dataproc/cleaning.go
@@ -1,4 +1,4 @@
-package main
+package dataproc
 
 import (
 	"encoding/json"
@@ -9,9 +9,7 @@ import (
 	"github.com/icza/s2prot/rep"
 )
 
-// type CleanedReplay struct {
-// 	header rep.Header
-// }
+// TODO: Introduce logging.
 
 func deleteUnusedObjects(replayData *rep.Rep) (data.CleanedReplay, bool) {
 
@@ -100,11 +98,15 @@ func deleteUnusedObjects(replayData *rep.Rep) (data.CleanedReplay, bool) {
 		// Accessing toon data by Golang magic:
 		toon := player.Struct["toon"]
 		intermediateJSON, err := json.Marshal(&toon)
+
+		// TODO: Error logging and handling
 		if err != nil {
 			return data.CleanedReplay{}, false
 		}
 		var unmarshalledData interface{}
 		err = json.Unmarshal(intermediateJSON, &unmarshalledData)
+
+		// TODO: Error logging and handling
 		if err != nil {
 			return data.CleanedReplay{}, false
 		}
@@ -204,20 +206,5 @@ func deleteUnusedObjects(replayData *rep.Rep) (data.CleanedReplay, bool) {
 		TrackerEvtsErr:    justTrackerEvtsErr,
 	}
 
-	// TODO: Initialize structs defined in custom_types directory
-
-	// TODO: Define for loops that will be checking different event types and not creating instances if the event type is unwanted
-	// Good example of that will be some of the chat events that are in messageEvents.
-
-	// TODO: Initialize my own type of CleanedReplay only with the fields that are needed.
-
 	return cleanedReplay, true
-}
-
-func anonymizeReplayData(replayData *rep.Rep) *rep.Rep {
-
-	// TODO: Anonymize the information about players.
-	// This needs to be done by calling some external file and / or memory which will be holding persistent information about all of the players.
-
-	return replayData
 }
