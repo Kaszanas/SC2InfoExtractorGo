94a8150eb279c2a0b0f363dffca945821d167515
Andrzej Białecki <kaszanas@interia.pl>
2021-02-19 10:04:19 +0100

    Defined TODOs, changed docstring

--- settings/delete_fields/user_options.go
+++ settings/delete_fields/user_options.go
@@ -1,6 +1,6 @@
 package settings
 
-// UserOptions are field names that will be deleted from the final JSON file that is exported
+// UserOptions are field names that will be deleted from rep.Rep.GameEvts game event that is of type "evtTypeName": "UserOptions"
 var UserOptions = []string{
 	"cameraFollow",
 	"debugPauseEnabled",
