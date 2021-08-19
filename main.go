50104871761a7ffd533e389ec147508055d54d6b
Andrzej Białecki <kaszanas@interia.pl>
2021-08-19 19:33:32 +0200

    Added check for number of files and packages

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 	inputDirectory := flag.String("input", "./DEMOS/Input", "Input directory where .SC2Replay files are held.")
 	// interDirectory := flag.String("inter", "./Demos/Intermediate", "Intermediate directory where .json files will be stored before bzip2 compression.")
 	outputDirectory := flag.String("output", "./DEMOS/Output", "Output directory where compressed bzip2 packages will be stored.")
-	numberOfPackagesFlag := flag.Int("number_of_packaged_files", 100, "Provide a number of files to be packaged to be compressed into a zip archive. Please remember that this number need to be lower than the number of processed files.")
+	numberOfPackagesFlag := flag.Int("number_of_packages", 1, "Provide a of packages to to be created and compressed into a zip archive. Please remember that this number needs to be lower than the number of processed files.")
 
 	performIntegrityCheckFlag := flag.Bool("integrity_check", true, "If the software is supposed to check the hardcoded integrity checks for the provided replays")
 	performValidityCheckFlag := flag.Bool("validity_check", true, "Provide if the tool is supposed to use hardcoded validity checks and verify if the replay file variables are within 'common sense' ranges.")
@@ -112,7 +112,14 @@ func main() {
 
 	// Getting list of absolute paths for files from input directory:
 	listOfInputFiles := utils.ListFiles(absolutePathInputDirectory, ".SC2Replay")
-	numberOfFilesInPackage := int(math.Ceil(float64(len(listOfInputFiles)) / float64(numberOfPackages)))
+	lenListOfInputFiles := len(listOfInputFiles)
+	if lenListOfInputFiles < numberOfPackages {
+		log.WithFields(log.Fields{
+			"lenListOfInputFiles": lenListOfInputFiles,
+			"numberOfPackages":    numberOfPackages}).Error("Higher number of packages than input files, closing the program.")
+		os.Exit(1)
+	}
+	numberOfFilesInPackage := int(math.Ceil(float64(lenListOfInputFiles) / float64(numberOfPackages)))
 	listOfChunksFiles := chunkSlice(listOfInputFiles, numberOfFilesInPackage)
 
 	// Register a custom compressor:
