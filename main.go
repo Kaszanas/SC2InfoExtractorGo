3d7704e1e6e7ea589b67fe51f3c77f90b8c6514a
Andrzej Białecki <kaszanas@interia.pl>
2021-09-01 02:23:30 +0200

    Switched the default bool CLI flags to false

--- main.go
+++ main.go
@@ -23,21 +23,21 @@ func main() {
 	outputDirectory := flag.String("output", "./DEMOS/Output", "Output directory where compressed zip packages will be saved.")
 	numberOfPackagesFlag := flag.Int("number_of_packages", 1, "Provide a number of packages to be created and compressed into a zip archive. Please remember that this number needs to be lower than the number of processed files.")
 
-	performIntegrityCheckFlag := flag.Bool("integrity_check", true, "If the software is supposed to check the hardcoded integrity checks for the provided replays")
-	performValidityCheckFlag := flag.Bool("validity_check", true, "Provide if the tool is supposed to use hardcoded validity checks and verify if the replay file variables are within 'common sense' ranges.")
+	performIntegrityCheckFlag := flag.Bool("integrity_check", false, "If the software is supposed to check the hardcoded integrity checks for the provided replays")
+	performValidityCheckFlag := flag.Bool("validity_check", false, "Provide if the tool is supposed to use hardcoded validity checks and verify if the replay file variables are within 'common sense' ranges.")
 
 	// TODO: Write the docs for other game modes:
 	gameModeCheckFlag := flag.Int("game_mode", 0b1111111111, "Provide which game mode should be included from the processed files in a format of a binary flag: AllGameModes: 0b1111111111")
 
 	// Other compression methods than Deflate need to be registered further down in the code:
 	compressionMethodFlag := flag.Int("compression_method", 8, "Provide a compression method number, default is 8 'Deflate', other compression methods need to be registered manually in code.")
-	localizeMapsBoolFlag := flag.Bool("localize_maps", true, "Set to false if You want to keep the original (possibly foreign) map names.")
+	localizeMapsBoolFlag := flag.Bool("localize_maps", false, "Set to false if You want to keep the original (possibly foreign) map names.")
 	localizationMappingFileFlag := flag.String("localized_maps_file", "./operation_files/output.json", "Specify a path to localization file containing {'ForeignName': 'EnglishName'} of maps.")
 
-	performCleanupFlag := flag.Bool("perform_cleanup", true, "Provide if the tool is supposed to perform the cleaning functions within the processing pipeline.")
-	performAnonymizationFlag := flag.Bool("perform_anonymization", true, "Provide if the tool is supposed to perform the anonymization functions within the processing pipeline.")
+	performCleanupFlag := flag.Bool("perform_cleanup", false, "Provide if the tool is supposed to perform the cleaning functions within the processing pipeline.")
+	performAnonymizationFlag := flag.Bool("perform_anonymization", false, "Provide if the tool is supposed to perform the anonymization functions within the processing pipeline.")
 
-	processWithMultiprocessingFlag := flag.Bool("with_multiprocessing", true, "Provide if the processing is supposed to be perform with maximum amount of available cores. If set to false, the program will use one core.")
+	processWithMultiprocessingFlag := flag.Bool("with_multiprocessing", false, "Provide if the processing is supposed to be perform with maximum amount of available cores. If set to false, the program will use one core.")
 
 	logLevelFlag := flag.Int("log_level", 4, "Provide a log level from 1-7. Panic - 1, Fatal - 2, Error - 3, Warn - 4, Info - 5, Debug - 6, Trace - 7")
 	performCPUProfilingFlag := flag.String("with_cpu_profiler", "", "Set path to the file where pprof cpu profiler will save its information. If this is empty no profiling is performed.")
