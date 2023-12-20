68a9a8855de4a936dedb759c462f39f47c21bc56
Kaszanas <andrzej.bialecki94@gmail.com>
2023-12-20 21:21:36 +0100

    Deleted old code, using GetTestLogsDirectory

--- settings/test_settings.go
+++ settings/test_settings.go
@@ -4,20 +4,12 @@ import (
 	"path/filepath"
 )
 
-// TODO: This should be set in some sort of settings:
-// This should also use environment variables:
-var TEST_LOGS_DIR = "./test_files/test_logs/"
-var TEST_LOCALIZATION_FILE_PATH = "./test_files/test_map_mapping/output.json"
-
-var TEST_INPUT_DIR = "./test_files/test_replays"
-var TEST_OUTPUT_DIR = "./test_files/test_replays_output/"
-var TEST_PROCESSED_FAILED_LOG = TEST_LOGS_DIR + "processed_failed_0.log"
-
 // REVIEW: Is it better to have an environment variable for the workspace directory?
 // Or is it better to have that in a .env file?
 // How to load a .env file? that is outside of the package?
 func GetWorkspaceDirectory() (string, error) {
 
+	// REVIEW: Will this consistently point to the workspace?
 	workspace, err := filepath.Abs("../")
 	if err != nil {
 		return "", err
@@ -71,12 +63,12 @@ func GetTestOutputDirectory() (string, error) {
 }
 
 func GetTestProcessedFailedLog() (string, error) {
-	workspace, err := GetWorkspaceDirectory()
+	logsDirectory, err := GetTestLogsDirectory()
 	if err != nil {
 		return "", err
 	}
 
-	processedFailedLog := filepath.Join(workspace, "test_files/test_logs/processed_failed_0.log")
+	processedFailedLog := filepath.Join(logsDirectory, "processed_failed_0.log")
 
 	return processedFailedLog, nil
 }
