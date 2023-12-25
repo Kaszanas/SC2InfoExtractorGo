3531a6874c510a70a9d71cdf2c23c5f7b2628ed7
Kaszanas <andrzej.bialecki94@gmail.com>
2023-12-25 01:04:20 +0100

    Refactored download_utils

--- test_utils/download_utils.go
+++ test_utils/download_utils.go
@@ -11,10 +11,6 @@ import (
 	"github.com/Kaszanas/SC2InfoExtractorGo/settings"
 )
 
-// TODO: This is only a sample, the repository name will change:
-var TEST_FILES_ARCHIVE = "https://github.com/Kaszanas/SC2InfoExtractorGo/releases/latest/download/sc2reset_test_files.zip"
-var TEST_ARCHIVE_FILEPATH = "test_files.zip"
-
 func TestFileSetup() error {
 	testFilesDirectory, err := settings.GetTestFilesDirectory()
 	if err != nil {
@@ -26,7 +22,9 @@ func TestFileSetup() error {
 		return err
 	}
 
-	archive, err := DownloadTestFiles(TEST_FILES_ARCHIVE, testFilesDirectory)
+	downloadFilepath := filepath.Join(testFilesDirectory, settings.TEST_ARCHIVE_FILEPATH)
+
+	archive, err := DownloadFile(downloadFilepath, settings.TEST_FILES_ARCHIVE)
 	if err != nil {
 		return err
 	}
@@ -47,35 +45,34 @@ func UnpackArchive(archivePath string, destinationDir string) error {
 	}
 	defer reader.Close()
 
-	for _, file := range reader.File {
-		filePath := filepath.Join(destinationDir, file.Name)
+	for _, archivedFile := range reader.File {
+		pathToExtractedFile := filepath.Join(destinationDir, archivedFile.Name)
 
 		// Skip if the file already exists:
-		if _, err := os.Stat(filePath); !os.IsNotExist(err) {
+		if _, err := os.Stat(pathToExtractedFile); !os.IsNotExist(err) {
 			continue
 		}
 
 		// Create file and copy contents
-		err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm)
+		err := os.MkdirAll(filepath.Dir(pathToExtractedFile), os.ModePerm)
 		if err != nil {
 			return err
 		}
 
-		outFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+		extractedFile, err := os.OpenFile(pathToExtractedFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, archivedFile.Mode())
 		if err != nil {
 			return err
 		}
-		defer outFile.Close()
+		defer extractedFile.Close()
 
-		// Get the archived file:
-		rc, err := file.Open()
+		archivedFileReadCloser, err := archivedFile.Open()
 		if err != nil {
 			return err
 		}
-		defer rc.Close()
+		defer archivedFileReadCloser.Close()
 
 		// Copy the file to the destination outside of the archive:
-		_, err = io.Copy(outFile, rc)
+		_, err = io.Copy(extractedFile, archivedFileReadCloser)
 		if err != nil {
 			return err
 		}
@@ -84,45 +81,26 @@ func UnpackArchive(archivePath string, destinationDir string) error {
 	return nil
 }
 
-func DownloadTestFiles(testFilesURL string, downloadDir string) (*os.File, error) {
-
-	testFilesDirectory, err := settings.GetTestFilesDirectory()
-	if err != nil {
-		return nil, err
-	}
-
-	downloadFilepath := filepath.Join(testFilesDirectory, TEST_ARCHIVE_FILEPATH)
-
-	archive, err := DownloadFile(downloadFilepath, testFilesURL)
-	if err != nil {
-		return nil, err
-	}
-
-	return archive, nil
-
-}
-
 // DownloadFile sends a GET request to the url and writes the response body to the filepath.
 func DownloadFile(downloadFilepath string, url string) (*os.File, error) {
 
 	// Get the data
-	resp, err := http.Get(url)
+	response, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
+	defer response.Body.Close()
 
 	// TODO: Check if the response is of file type.
-
 	// Create the file
-	out, err := os.Create(downloadFilepath)
+	outputFile, err := os.Create(downloadFilepath)
 	if err != nil {
 		return nil, err
 	}
-	defer out.Close()
+	defer outputFile.Close()
 
 	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	return out, err
+	_, err = io.Copy(outputFile, response.Body)
+	return outputFile, err
 
 }
