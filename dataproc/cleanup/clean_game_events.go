3962cecf623ec8a8173018627b2676d0665c5774
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2025-01-29 18:25:29 +0100

    Drafted clean_game_events functions

--- dataproc/cleanup/clean_game_events.go
+++ dataproc/cleanup/clean_game_events.go
@@ -110,66 +110,153 @@ func CleanGameEvents(replayData *rep.Rep) []s2prot.Struct {
 
 		eventTypeName := gameEventStruct["evtTypeName"].(string)
 		if eventTypeName == "Cmd" {
-
-			// gameEventAbility := gameEventStruct["abil"]
-			// if gameEventAbility != nil {
-			// 	gameEventAbility := gameEventAbility.(s2prot.Struct)
-			// 	abilityLink := gameEventAbility["abilLink"].(int64)
-			// 	abilityCmdIndex := gameEventAbility["abilCmdIndex"].(int64)
-			// 	abilCmdData := gameEventAbility["abilCmdData"]
-			// 	if abilCmdData != nil {
-			// 		log.Info("abilCmdData is not nil")
-			// 	}
-			// 	log.WithField("abilityLink", abilityLink).Debug("Got ability link")
-			// 	log.WithField("abilityCmdIndex", abilityCmdIndex).Debug("Got ability cmdIndex")
-			// 	log.WithField("abilCmdData", abilCmdData).Debug("Got ability cmdData")
-
-			// TODO: load this from balance data where possible
-			// REVIEW: What to do if no balance data is available?
-			// should this break the processing?
-
-			// TODO: I have found the proper string ability name and the string ability command.
-			// Either replace the values with strings,
-			// maybe a better option is to add more fields such as abilityName and abilityCommandName
-			// }
-
-			// gameEventData := gameEventStruct["data"]
-			// if gameEventData != nil {
-			// 	gameEventData := gameEventStruct["data"].(s2prot.Struct)
-
-			// 	// Target point needs to be translated to the true coordinates:
-			// 	targetPoint := gameEventData["targetPoint"]
-			// 	// Scale x,y,z value to match the true coordinates:
-
-			// 	if targetPoint != nil {
-			// 		targetUnit := gameEventData["targetPoint"].(s2prot.Struct)
-			// 		log.WithField("targetUnit", targetUnit).Debug("Got target unit")
-
-			// 	}
-
-			// TODO: SNAPSHOT POINTS SHOULD BE TRANSLATED TO TRUE CORRDINATES SEPARATELY:
-			// snapshotPoint := gameEventData["snapshotPoint"].(s2prot.Struct)
-			// snapshotPointX := float64(snapshotPoint["x"].(int64)) / 8192
-			// snapshotPointY := float64(snapshotPoint["y"].(int64)) / 4096
-			// snapshotPointZ := float64(snapshotPoint["z"].(int64)) / 4096
-
-			// gameEventStruct["data"].(s2prot.Struct)["snapshotPoint"].(s2prot.Struct)["x"] = snapshotPointX
-			// gameEventStruct["data"].(s2prot.Struct)["snapshotPoint"].(s2prot.Struct)["y"] = snapshotPointY
-			// gameEventStruct["data"].(s2prot.Struct)["snapshotPoint"].(s2prot.Struct)["z"] = snapshotPointZ
-
-			// }
-
-			gameEventFlagBitmask := gameEvent.Int("cmdFlags")
-			commandFlags := getCommandType(gameEventFlagBitmask)
-
-			log.WithField("CommandType", commandFlags).Debug("Got command type")
-			log.Debug("GameEventInt: ", gameEventFlagBitmask)
-
-			gameEventStruct["cmdFlags"] = commandFlags
+			gameEventStruct = cleanCmdEvent(gameEvent, gameEventStruct)
 		}
 
 		gameEventsStructs = append(gameEventsStructs, gameEventStruct)
 	}
 	log.Info("Defined cleanGameEvents struct")
 	return gameEventsStructs
+}
+
+func cleanCmdEvent(
+	gameEvent s2prot.Event,
+	gameEventStruct s2prot.Struct,
+) s2prot.Struct {
+
+	gameEventAbility := gameEventStruct["abil"]
+	if gameEventAbility != nil {
+		gameEventAbility := gameEventStruct["abil"].(s2prot.Struct)
+		abilityData := getAbilityData(gameEventAbility)
+		gameEventAbility["abilityData"] = abilityData
+		gameEventStruct["abil"] = gameEventAbility
+	}
+
+	// Recalculating snapshotPoint for a TargetUnit:
+	gameEventData := gameEventStruct["data"]
+	if gameEventData != nil {
+		gameEventData := gameEventStruct["data"].(s2prot.Struct)
+		// Recalculating SnapshotPoint for a TargetUnit:
+		gameEventData = recalculateSnapshotPoint(gameEventData)
+
+		// Recalculating TargetPoint:
+		gameEventData = recalculateTargetPoint(gameEventData)
+		gameEventStruct["data"] = gameEventData
+	}
+
+	// Getting command flags:
+	gameEventStruct = getCmdFlags(gameEvent, gameEventStruct)
+
+	return gameEventStruct
+}
+
+// recalculateTargetPoint recalculates the targetPoint
+// coordinates and returns the mutated gameEventData.
+func recalculateTargetPoint(gameEventData s2prot.Struct) s2prot.Struct {
+
+	targetPoint := gameEventData["targetPoint"]
+	if targetPoint != nil {
+		targetPoint := gameEventData["targetPoint"].(s2prot.Struct)
+
+		targetPointX := float64(targetPoint["x"].(int64)) / 8192
+		targetPointY := float64(targetPoint["y"].(int64)) / 8192
+		targetPointZ := float64(targetPoint["z"].(int64)) / 8192
+
+		targetPoint["x"] = targetPointX
+		targetPoint["y"] = targetPointY
+		targetPoint["z"] = targetPointZ
+		gameEventData["targetPoint"] = targetPoint
+		return gameEventData
+	}
+
+	return gameEventData
+}
+
+// recalculateSnapshotPoint recalculates the snapshotPoint coordinates
+// for the targetUnit and returns the mutated gameEventData.
+func recalculateSnapshotPoint(gameEventData s2prot.Struct) s2prot.Struct {
+
+	targetUnit := gameEventData["targetUnit"]
+	if targetUnit != nil {
+		targetUnit := gameEventData["targetUnit"].(s2prot.Struct)
+		snapshotPoint := targetUnit["snapshotPoint"]
+		if snapshotPoint != nil {
+			snapshotPoint := targetUnit["snapshotPoint"].(s2prot.Struct)
+
+			snapshotPointX := float64(snapshotPoint["x"].(int64)) / 8192
+			snapshotPointY := float64(snapshotPoint["y"].(int64)) / 8192
+			snapshotPointZ := float64(snapshotPoint["z"].(int64)) / 8192
+
+			snapshotPoint["x"] = snapshotPointX
+			snapshotPoint["y"] = snapshotPointY
+			snapshotPoint["z"] = snapshotPointZ
+			targetUnit["snapshotPoint"] = snapshotPoint
+		}
+		gameEventData["targetUnit"] = targetUnit
+		return gameEventData
+	}
+	return gameEventData
+}
+
+// getCmdFlags translates the integer bitmask to a string array,
+// and returns the mutated gameEventStruct.
+func getCmdFlags(
+	gameEvent s2prot.Event,
+	gameEventStruct s2prot.Struct,
+) s2prot.Struct {
+
+	gameEventFlagBitmask := gameEvent.Int("cmdFlags")
+	commandFlags := getCommandType(gameEventFlagBitmask)
+
+	log.WithField("CommandType", commandFlags).Debug("Got command type")
+	log.Debug("GameEventInt: ", gameEventFlagBitmask)
+
+	gameEventStruct["cmdFlags"] = commandFlags
+	return gameEventStruct
+}
+
+// getAbilityData acquires the ability data from the gameEventAbility struct.
+// Mutates the gameEventAbility struct and returns it containing
+// a human readable ability name and command name.
+func getAbilityData(gameEventAbility s2prot.Struct) s2prot.Struct {
+
+	// TODO: load this from balance data where possible
+	// REVIEW: What to do if no balance data is available?
+	// should this break the processing?
+	// TODO: I have found the proper string ability name and the string ability command.
+	// Either replace the values with strings,
+	// maybe a better option is to add more fields such as abilityName and abilityCommandName
+	gameEventAbility = getAbilityName(gameEventAbility)
+	gameEventAbility = getAbilityCommandName(gameEventAbility)
+
+	return gameEventAbility
+}
+
+// getAbilityName acquires the ability name, mutates the gameEventAbility and returns it.
+func getAbilityName(gameEventAbility s2prot.Struct) s2prot.Struct {
+
+	// TODO: This requires lookup to the game version balance data
+
+	// 	gameEventAbility := gameEventAbility.(s2prot.Struct)
+	// 	abilityLink := gameEventAbility["abilLink"].(int64)
+
+	return gameEventAbility
+}
+
+// getAbilityCommandName acquires the ability specific command name,
+// mutates the gameEventAbility and returns it.
+func getAbilityCommandName(gameEventAbility s2prot.Struct) s2prot.Struct {
+
+	// TODO: This requires lookup to the game version balance data
+
+	// 	abilityCmdIndex := gameEventAbility["abilCmdIndex"].(int64)
+	// 	abilCmdData := gameEventAbility["abilCmdData"]
+	// 	if abilCmdData != nil {
+	// 		log.Info("abilCmdData is not nil")
+	// 	}
+	// 	log.WithField("abilityLink", abilityLink).Debug("Got ability link")
+	// 	log.WithField("abilityCmdIndex", abilityCmdIndex).Debug("Got ability cmdIndex")
+	// 	log.WithField("abilCmdData", abilCmdData).Debug("Got ability cmdData")
+
+	return gameEventAbility
 }
